c6dde61e3ba5b6f44387582cc15e93e4a457d1c6
Joshua Price <2855417+WhistlePayer@users.noreply.github.com>
2018-11-18 20:54:22 -0500

    Implement movement logic

--- src/Chesserole/Chess.hs
+++ src/Chesserole/Chess.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -8,10 +9,15 @@
 
 module Chesserole.Chess where
 
-import Data.Char     (toLower)
-import Data.List     (intercalate)
-import Linear        (V2(..))
-import Linear.Affine (Point(..))
+import Control.Applicative  (ZipList(..))
+import Control.Monad        (join, mfilter)
+import Data.Char            (toLower)
+import Data.Functor.Compose (Compose(..))
+import Data.List            (intercalate)
+import Data.Maybe           (isNothing, mapMaybe, maybeToList)
+import Data.Foldable        (asum)
+import Linear               (V2(..))
+import Linear.Affine        ((.+^), Point(..))
 
 -- TODO: Organize
 
@@ -19,6 +25,10 @@ import Linear.Affine (Point(..))
 
 data Color = White | Black
   deriving (Eq, Ord, Show, Enum)
+
+invColor :: Color -> Color
+invColor White = Black
+invColor Black = White
 
 data PieceType = Pawn | Knight | Bishop | Rook | Queen | King
   deriving (Eq, Ord, Show, Enum)
@@ -28,32 +38,40 @@ data Piece = Piece
   , pieceType :: PieceType
   } deriving (Eq, Ord, Show)
 
-newtype Board = Board { boardPieces :: [[Maybe Piece]] }
+newtype Board' a = Board { boardPieces :: [[a]] }
   deriving (Eq, Show)
+  deriving (Functor, Applicative) via (Compose ZipList ZipList)
+
+type Board = Board' (Maybe Piece)
 
 --------------------------------------------------------------------------------
 
--- TODO: Square: 'Point' newtype or product type?
+-- TODO: @Point V2 IntMod8@?
 newtype Square = Square' { squarePos :: Point V2 Int }
   deriving (Eq, Show)
 
--- TODO: (rank, file) or (file, rank)?
 {-# COMPLETE Square #-}
 pattern Square :: Int -> Int -> Square
-pattern Square{squareRank, squareFile} <- Square' (P (V2 squareRank squareFile))
+pattern Square{squareFile, squareRank} <- Square' (P (V2 squareFile squareRank))
 
 square :: Int -> Int -> Maybe Square
-square rank file
-  | inBounds rank && inBounds file = Just (Square' (P (V2 rank file)))
+square file rank
+  | inBounds file && inBounds rank = Just (Square' (P (V2 file rank)))
   | otherwise = Nothing
 
 squareP :: Point V2 Int -> Maybe Square
-squareP vec@(P (V2 rank file))
-  | inBounds rank && inBounds file = Just (Square' vec)
+squareP pt@(P (V2 file rank))
+  | inBounds file && inBounds rank = Just (Square' pt)
   | otherwise = Nothing
+
+squareModP :: Point V2 Int -> Square
+squareModP = Square' . fmap (`rem` 8)
 
 inBounds :: Int -> Bool
 inBounds n = 0 <= n && n < 8
+
+shiftSquare :: Square -> V2 Int -> Maybe Square
+shiftSquare (Square' pt) v = squareP $ pt .+^ v
 
 data CastleSide = QueenSide | KingSide
   deriving (Eq, Ord, Show, Enum)
@@ -67,7 +85,7 @@ data Game = Game
   { gameBoard :: Board
   , gamePlayer :: Color
   , gameCastling :: [CastleType] -- TODO: Set?
-  , gamePassant :: Maybe Square
+  , gameEnPassant :: Maybe Square
   , gameClock :: Int
   , gameMoves :: Int
   } deriving (Eq, Show)
@@ -85,17 +103,29 @@ initialBoard = Board $
       pawnRank = replicate 8 Pawn
       backRank = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
 
-getPiece :: Square -> Board -> Maybe Piece
-getPiece (Square rank file) (Board pieces) = pieces !! file !! rank
+initialGame :: Game
+initialGame = Game
+  { gameBoard = initialBoard
+  , gamePlayer = White
+  , gameCastling = CastleType <$> [White, Black] <*> [QueenSide, KingSide]
+  , gameEnPassant = Nothing
+  , gameClock = 0
+  , gameMoves = 0
+  }
 
-setPiece :: Square -> Maybe Piece -> Board -> Board
-setPiece (Square rank file) maypiece (Board pieces) =
-  Board $ modifyInList file (setInList rank maypiece) pieces
+--------------------------------------------------------------------------------
+
+getAtSquare :: Square -> Board' a -> a
+getAtSquare (Square file rank) (Board pieces) = pieces !! rank !! file
+
+setAtSquare :: Square -> Maybe Piece -> Board -> Board
+setAtSquare (Square file rank) maypiece (Board pieces) =
+  Board $ modifyInList rank (setInList file maypiece) pieces
 
 forceMovePiece :: Square -> Square -> Board -> Board
 forceMovePiece from to board =
-  let maypiece = getPiece from board
-  in setPiece from Nothing $ setPiece to maypiece board
+  let maypiece = getAtSquare from board
+  in setAtSquare to maypiece $ setAtSquare from Nothing board
 
 --------------------------------------------------------------------------------
 
@@ -115,7 +145,7 @@ fen Game{..} = unwords
   [ fenBoard gameBoard
   , fenPlayer gamePlayer
   , fenCastle gameCastling
-  , fenPassant gamePassant
+  , fenEnPassant gameEnPassant
   , show gameClock
   , show gameMoves
   ]
@@ -163,17 +193,15 @@ fenCastleSide :: CastleSide -> String
 fenCastleSide QueenSide = "Q"
 fenCastleSide KingSide  = "K"
 
-fenPassant :: Maybe Square -> String
-fenPassant Nothing  = "-"
-fenPassant (Just s) = anSquare s
+fenEnPassant :: Maybe Square -> String
+fenEnPassant Nothing  = "-"
+fenEnPassant (Just s) = anSquare s
 
 --------------------------------------------------------------------------------
 
 data Move
   = Move Square Square
   | Castle CastleSide
-
---------------------------------------------------------------------------------
 
 anMove :: Move -> String
 anMove (Move from to) = anSquare from <> anSquare to
@@ -203,3 +231,108 @@ parseAnSquare :: String -> Maybe Square
 parseAnSquare [file, rank] = square (dist '1' rank) (dist 'a' file)
   where dist x y = fromEnum y - fromEnum x
 parseAnSquare _ = Nothing
+
+--------------------------------------------------------------------------------
+
+validMovesFrom :: Game -> Square -> [Square]
+validMovesFrom game@Game{..} from
+  =  standardMovement gameBoard from
+  <> pawnMovement game from
+  <> castlingMovement game from
+
+standardMovement :: Board -> Square -> [Square]
+standardMovement board from = do
+  Piece{..} <- maybeToList $ getAtSquare from board
+  move <- pieceMoves pieceType
+  let onBoard = mapMaybe (shiftSquare from) move
+  takeWhileAnd (blockTest board pieceColor) onBoard
+
+blockTest :: Board -> Color -> Square -> Maybe Bool
+blockTest board color from = (color /=) . pieceColor <$> getAtSquare from board
+
+takeWhileAnd :: (a -> Maybe Bool) -> [a] -> [a]
+takeWhileAnd f = foldr go []
+  where
+    go x r = case f x of
+      Nothing    -> x : r
+      Just True  -> [x]
+      Just False -> []
+
+pieceMoves :: PieceType -> [[V2 Int]]
+pieceMoves = \case
+  Pawn -> [] -- Pawn movement must be implemented seperately
+  Knight -> [V2 2 1, V2 1 2] >>= mirrorMovesOrth . pure
+  Bishop -> bishopMoves
+  Rook -> rookMoves
+  Queen -> bishopMoves <> rookMoves
+  King -> pure <$> (V2 <$> [-1..1] <*> [-1..1])
+  where
+    bishopMoves = mirrorMovesOrth (join V2 <$> [1..7])
+    rookMoves = mirrorMovesDiag (V2 0 <$> [1..7])
+
+mirrorMovesWith :: (V2 a -> V2 a) -> (V2 a -> V2 a) -> [V2 a] -> [[V2 a]]
+mirrorMovesWith mirror1 mirror2 moves =
+  fmap (<$> moves) $ (.) <$> [id, mirror1] <*> [id, mirror2]
+
+mirrorMovesOrth :: [V2 Int] -> [[V2 Int]]
+mirrorMovesOrth =
+  mirrorMovesWith (\(V2 x y) -> V2 x (-y)) (\(V2 x y) -> V2 (-x) y)
+
+mirrorMovesDiag :: [V2 Int] -> [[V2 Int]]
+mirrorMovesDiag =
+  mirrorMovesWith (\(V2 x y) -> V2 y x) (\(V2 x y) -> V2 (-x) (-y))
+
+pawnMovement :: Game -> Square -> [Square]
+pawnMovement game@Game{..} from = do
+  Piece color Pawn <- maybeToList $ getAtSquare from gameBoard
+  asum [ pawnAdvance gameBoard from color
+       , pawnCapture gameBoard from color
+       , pawnDouble gameBoard from color
+       , pawnEnPassant game from
+       ]
+
+pawnAdvance :: Board -> Square -> Color -> [Square]
+pawnAdvance board from color
+  = maybeToList
+  . mfilter (isNothing . (`getAtSquare` board))
+  . shiftSquare from
+  $ pawnAdvanceMove color
+  where
+    pawnAdvanceMove White = V2 0 (-1)
+    pawnAdvanceMove Black = V2 0 1
+
+pawnCapture :: Board -> Square -> Color -> [Square]
+pawnCapture board from color
+  =   maybeToList
+  .   mfilter (capturable . (`getAtSquare` board))
+  .   shiftSquare from
+  =<< pawnAdvanceMove color
+  where
+    capturable = (Just (invColor color) ==) . fmap pieceColor
+    pawnAdvanceMove White = V2 <$> [-1,1] <*> [-1]
+    pawnAdvanceMove Black = V2 <$> [-1,1] <*> [1]
+
+-- TODO: Clean up
+pawnDouble :: Board -> Square -> Color -> [Square]
+pawnDouble board from color
+  = maybeToList
+  . mfilter ((valid &&) . isNothing . (`getAtSquare` board))
+  . shiftSquare from
+  $ pawnDoubleMove color
+  where
+    valid = unblocked && correctRank
+    unblocked
+      = isNothing $ (`getAtSquare` board) =<< shiftSquare from (pawnAdvanceMove color)
+    pawnAdvanceMove White = V2 0 (-1)
+    pawnAdvanceMove Black = V2 0 1
+    correctRank = pawnStartRank color == squareRank from
+    pawnStartRank White = 6
+    pawnStartRank Black = 1
+    pawnDoubleMove White = V2 0 (-2)
+    pawnDoubleMove Black = V2 0 2
+
+pawnEnPassant :: Game -> Square -> [Square]
+pawnEnPassant _board _from = [] -- TODO
+
+castlingMovement :: Game -> Square -> [Square]
+castlingMovement _game _from = [] --TODO
